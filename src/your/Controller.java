0ef0dd0e7db976da2ba6c04858d695852392e972
JSOUTHWELLCODE <j14onathan@gmail.com>
2025-03-04 15:42:32 +0000

    We have rectangles and event listener for the choicebox

--- src/your/Controller.java
+++ src/your/Controller.java
@@ -1,14 +1,23 @@
 package your;
 
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
-
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.transform.Rotate;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+
 
 
 public class Controller implements Initializable {
@@ -39,6 +48,15 @@ public class Controller implements Initializable {
 
     private String[] algos = {"Merge Sort", "Insertion Sort",  "Bubble sort"};
 
+    @FXML
+    private HBox HBOX1;
+
+
+
+
+
+
+
 
 
 
@@ -48,7 +66,62 @@ public class Controller implements Initializable {
         mychoicebox1.getItems().addAll(algos);
         mychoicebox2.getItems().addAll(algos);
         mychoicebox3.getItems().addAll(algos);
+        DataSet data = new DataSet();
+
+        mychoicebox1.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            //looks for a change in the choicebox
+            public void changed(ObservableValue<? extends String> observableValue, String oldVal, String newVal) {
+                if (newVal != null){
+
+
+
+
+
+
+                    //int size = data.getarraySize();
+
+                   //starts at 0 and is incremented to add the other rectangels
+                    double xPosition = 0;
+                    ArrayList<Integer> array = data.getArray();
+
+                    for(int i = 0; i < data.getarraySize(); i++){
+
+
+
+                        // Nneed another for loop to go through the array and make the rectangles biger
+                        int sizerect = array.get(i);
+                        Rectangle rect = new Rectangle(2.5, sizerect , Color.BLACK);
+
+                        rect.setX(xPosition);
+                        rect.setY(0);
+                        xPosition += 3;
+//                        Rotate rotate = new Rotate();
+//                        rotate.setAngle(180);
+//                        rect.getTransforms().addAll(rotate);
+
+
+                        HBOX1.getChildren().add(rect);
+
+
+                    }
+                    // ensures hbox updates
+//                    HBOX1.setAlignment(Pos.CENTER);
+                    HBOX1.setScaleY(-1);
+                    HBOX1.layout();
+
+
+                }
+            }
+        });
+
 
 
     }
+
+
+
+
+
+
 }
--- src/your/Main.java
+++ src/your/Main.java
@@ -6,7 +6,9 @@ import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,7 +18,7 @@ public class Main extends Application {
             //Loads the XML file
             Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
 
-           // Group root = new Group();
+
             Scene scene = new Scene(root);
 
             //set the height and width of the scene
@@ -24,7 +26,19 @@ public class Main extends Application {
             String css = this.getClass().getResource("application.css").toExternalForm();
             scene.getStylesheets().add(css);
 
+            VBox vbox = new VBox();
+
+            Rectangle rectangle = new Rectangle(10,10,Color.BLUE);
+
+            vbox.getChildren().add(rectangle);
+
+
+
+
+
             stage.setScene(scene);
+
+
 
             stage.show();
 
